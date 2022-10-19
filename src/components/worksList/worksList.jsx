d729e50d8dfbf3eeff2297ebe0bc2818235866f8
Marek Kalus <marektg2@gmail.com>
2022-10-19 15:07:45 +0200

    refactor db structure

--- src/components/worksList/worksList.jsx
+++ src/components/worksList/worksList.jsx
@@ -1,8 +1,5 @@
-import niewymierne from 'data/liczbyNiewymierne';
 import React from 'react'
-import naturalne from "../../data/liczby naturalne";
-import calkowiteIWymierne from '../../data/liczbyCalkowiteIWymierna';
-import rozDzies from '../../data/rozwiniecieDziesietne';
+import liczby from "../../data/liczby";
 import pierwiastki from '../../data/pierwiastki';
 import potegi from '../../data/potegi'
 import WievTask from './wievTask';
@@ -12,23 +9,15 @@ const WorksList = ({ model }) => {
     let data;
     switch (model) {
         case "1":
-            data = naturalne;
+            data = liczby;
             return (<WievTask data={data} model={model } />);
         case "2":
-            data = calkowiteIWymierne;
-            return (<WievTask data={data} model={model} />);
-        case "3":
-            data = niewymierne;
-            return (<WievTask data={data} model={model} />);
-        case "4":
-            data = rozDzies;
-            return (<WievTask data={data} model={model} />);
-        case "5":
             data = pierwiastki;
             return (<WievTask data={data} model={model} />);
-        case "6":
+        case "3":
             data = potegi;
             return (<WievTask data={data} model={model} />);
+        
         
         default: return (<div></div>);
         
--- src/pages/srednia/lrzeczywiste/lrzeczywiste.jsx
+++ src/pages/srednia/lrzeczywiste/lrzeczywiste.jsx
@@ -3,6 +3,7 @@ import styles from './lrzeczywiste.module.scss';
 import TopNavi from 'components/topNavi/topNavi';
 import LeftNavi from 'components/leftNavi/leftNavi';
 import WorksList from 'components/worksList/worksList';
+import { MathJax } from 'better-react-mathjax';
 // import { current } from '@reduxjs/toolkit';
 
 const Lrzeczywiste = () => {
@@ -32,17 +33,14 @@ const Lrzeczywiste = () => {
                     <h2 className={tytul}>Liczby rzeczywiste </h2>
                     <ul className={podmenu}>
                         <li id="1" key="1"><button onClick={renderTask}>Liczby naturalne</button></li>
-                        <li id="2" key="2"><button onClick={renderTask}>Liczby całkowite. Liczby wymierne</button></li>
-                        <li id="3" key="3"><button onClick={renderTask}>Liczby niewymierne</button></li>
-                        <li id="4" key="4"><button onClick={renderTask}>Rozwinięcie dziesiętne liczby rzeczywistej</button></li>
-                        <li id="5" key="5"><button onClick={renderTask}>Pierwiasteki</button></li>
-                        <li id="6" key="6"><button onClick={renderTask}>Potęgi</button></li>
-                        <li id="7" key="7"><button onClick={renderTask}>Notacja wykładnicza</button></li>
-                        <li id="8" key="8"><button onClick={renderTask}>Logarytm i jego własności</button></li>
-                        <li id="9" key="9"><button onClick={renderTask}>Procenty</button></li>
-                        <li id="10" key="10"><button onClick={renderTask}>Powtórzenie</button></li>
-                        <li id="11" key="11"><button onClick={renderTask}>Zadania testowe</button></li>
-                        <li id="12" key="12"><button onClick={renderTask}>To było na maturze</button></li>
+                        <li id="2" key="2"><button onClick={renderTask}>Pierwiasteki</button></li>
+                        <li id="3" key="3"><button onClick={renderTask}>Potęgi</button></li>
+                        <li id="4" key="4"><button onClick={renderTask}><MathJax>{"\\(\\star\\)"}</MathJax>Notacja wykładnicza</button></li>
+                        <li id="5" key="5"><button onClick={renderTask}>Logarytm i jego własności</button></li>
+                        <li id="6" key="6"><button onClick={renderTask}>Procenty</button></li>
+                        <li id="7" key="7"><button onClick={renderTask}>Powtórzenie</button></li>
+                        <li id="8" key="8"><button onClick={renderTask}>Zadania testowe</button></li>
+                        <li id="9" key="9"><button onClick={renderTask}>To było na maturze</button></li>
                     </ul>
                     <div className={tasks}>
                         <h3 className={rodzajzad}>{title}</h3>
